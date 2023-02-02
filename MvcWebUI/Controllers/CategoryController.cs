e6901cc66b514d1de7a28cbd30f40cfd5fb0db7a
ozgun balta <ozgunbalta@Icloud.com>
2023-02-02 22:27:06 +0300

    Updated

--- MvcWebUI/Controllers/CategoryController.cs
+++ MvcWebUI/Controllers/CategoryController.cs
@@ -15,7 +15,7 @@ namespace MvcWebUI.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index", "Product");
         }
 
         [Route("getcategories")]
